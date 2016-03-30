3ce0ba69bd10fc5c4c2f4d70864e7e007b2db3f1
tracycheng2015 <756593537@qq.com>
2016-03-30 15:57:05 +0800

    修改程序

---
+++ include/init.php
@@ -0,0 +1,23 @@
+<?php 
+
+
+$user = $_SERVER['PHP_AUTH_USER'];
+$pass = $_SERVER['PHP_AUTH_PW'];
+
+if(isset($user) && isset($pass)){
+
+var_dump($user,$pass);
+echo "OK";
+$_SERVER['PHP_AUTH_USER'] = NULL;
+$_SERVER['PHP_AUTH_PW'] = NULL;
+
+} else {
+
+
+	header('WWW-Authenticate: Basic realm="USER LOGIN"');
+	header('HTTP/1.0 401 Unauthorized');
+ 
+
+}
+
+?>
