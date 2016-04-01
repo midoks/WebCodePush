1b944b68a0c88d92a62cc5d20f8ff82304edb3b4
midoks <midoks@163.com>
2016-04-01 14:23:33 +0800

    组织目录

--- include/init.php
+++ include/init.php
@@ -1,24 +1,28 @@
 <?php 
 
-$user = $_SERVER['PHP_AUTH_USER'];
-$pass = $_SERVER['PHP_AUTH_PW'];
-//$type = $_SERVER['AUTH_TYPE']; 
+define('WCP_ROOT', str_replace('\\', '/', dirname(dirname(__FILE__))));
+define('WCP_INC', WCP_ROOT.'/include');
+var_dump(WCP_INC);
 
-if(isset($user) && isset($pass)){
+// $user = $_SERVER['PHP_AUTH_USER'];
+// $pass = $_SERVER['PHP_AUTH_PW'];
+// //$type = $_SERVER['AUTH_TYPE']; 
 
-var_dump($user,$pass, $type);
-echo "OK";
-$_SERVER['PHP_AUTH_USER'] = "";
-$_SERVER['PHP_AUTH_PW'] = "";
+// if(isset($user) && isset($pass)){
 
-} else {
+// var_dump($user,$pass, $type);
+// echo "OK";
+// $_SERVER['PHP_AUTH_USER'] = "";
+// $_SERVER['PHP_AUTH_PW'] = "";
+
+// } else {
 
 
-	header('WWW-Authenticate: Basic realm="USER LOGIN"');
-	header('HTTP/1.0 401 Unauthorized');
+// 	header('WWW-Authenticate: Basic realm="USER LOGIN"');
+// 	header('HTTP/1.0 401 Unauthorized');
  
 
-}
+// }
 
 
 ?>
