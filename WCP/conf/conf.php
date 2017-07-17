341d8a22bb6c8d040a7b4bef28fa47cff6d0c41e
midoks <midoks@163.com>
2017-07-17 17:10:51 +0800

    update

--- WCP/conf/conf.php
+++ WCP/conf/conf.php
@@ -1,6 +1,6 @@
 <?php
 return array(
-	'filter'=>'svn,log,conf,DS_Store,metadata', 
+	'filter'=>'security,svn,error,config.,html.php,globaltop.,.log,local.inc', 
 	'hidden'=>'.,..,.DS_Store,metadata,.svn,log', 
 	'source_view'=>'html,php,sh,htaccess,log,json,sql,ini', 
 );
